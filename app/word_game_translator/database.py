5070d27298d16712fa693b011d7597851c9933a1
DVanam <hisokadara@gmail.com>
2025-05-14 14:41:58 +0300

    app/word_game_translator - Create derictoy by version googletrans==3.1.0a0

---
+++ app/word_game_translator/database.py
@@ -0,0 +1,68 @@
+import json
+import sqlite3
+import random
+from datetime import datetime
+
+class Database:
+    def __init__(self, db_name):
+        self.connection = sqlite3.connect(db_name)
+        self.cursor = self.connection.cursor()
+        self.create_table()
+
+        self.words_list = []
+        self.words_dict = dict()
+
+    def create_table(self):
+        """Создаем таблицу в базе данных для хранения данных."""
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS words
+                                       (id INTEGER PRIMARY KEY,
+                                       english TEXT,
+                                       russian TEXT)''')
+        self.connection.commit()
+
+    def add_word_in_db(self, english, russian):
+        """Добавление в таблицу базы даныз"""
+        self.cursor.execute("INSERT INTO words (english, russian) VALUES (?, ?)", (english, russian))
+        self.connection.commit()
+
+    def save_to_json(self, filename):
+        """Создание json file in database"""
+        self.cursor.execute("SELECT * FROM words")
+        words = self.cursor.fetchall()
+        word_dict = [{"russian": row[1], "english": row[2]} for row in words]
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump(word_dict, f, indent=2)
+
+    def load_from_json(self, filename):
+        """Загрузка json file. Чере json.load."""
+        with open(filename, "r") as f:
+            word_dict = json.load(f)
+            for i in range(len(word_dict)):
+                ru = word_dict[i]["russian"]
+                en = word_dict[i]["english"]
+                # setdefault -Уникальных значений. Без повторов.
+                self.words_dict.setdefault(ru, en)  # dict
+
+    def get_table_db(self):
+        # self.cursor.execute("SELECT * FROM words ORDER BY RANDOM() LIMIT 1")
+        self.cursor.execute("SELECT * FROM words")
+        word_rand_db = self.cursor.fetchall()
+        all_table_db = []
+        for item in range(len(word_rand_db)):
+            all_table_db.append(word_rand_db[item])
+        return all_table_db
+
+    def get_last_data(self):
+        """Получаем последний сохраненный набор данных из базы данных."""
+        self.cursor.execute("SELECT * FROM words ORDER BY id DESC LIMIT 1")
+        rowd = self.cursor.fetchone()
+        if rowd:
+            return rowd[1], rowd[2]
+        # return json.loads(result[0]) if result else []
+
+    def __del__(self):
+        """Close the database connection when the object is deleted."""
+        self.connection.close()
+
+
+
---
+++ app/word_game_translator/main.py
@@ -0,0 +1,112 @@
+import sys
+import random
+from datetime import datetime
+import csv
+from PySide6 import QtWidgets
+from PySide6.QtWidgets import QApplication, QMainWindow
+from googletrans import Translator
+
+from ui_game import Ui_MainWindow
+from ui_new_add import Ui_Dialog
+from database import Database
+
+class TranslatorApp(QMainWindow):
+    def __init__(self):
+        # QMainWindow.__init__(self)
+        super(TranslatorApp, self).__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.input_text = self.ui.input_text
+        self.output_text = self.ui.output_text
+        self.input_text.textChanged.connect(self.translate_text)
+
+        self.en_text = self.ui.en_le_text
+        self.en_text.returnPressed.connect(self.check_answer)
+        self.ru_text = self.ui.ru_qeustion
+
+        self.history_field = self.ui.check_qeust_ans_text
+
+        self.session_spin = self.ui.session_spinbox
+        self.start_button = self.ui.start_session
+        self.start_button.clicked.connect(self.start_session)
+        self.add_new_btn = self.ui.save_word_button
+        self.add_new_btn.clicked.connect(self.add_words_in_file_js_db)
+
+        self.translator = Translator()
+
+        self.database = Database('trans_database.db')
+        self.load_table_database()
+
+        self.database.load_from_json('words_js_file.json')
+        self.words = self.database.words_dict
+        
+        self.session_words = []
+        self.current_word = None
+        # self.show()
+
+    def load_table_database(self):
+        """Загрузка и отображение таблицы из сохраненных данных из базы данных."""
+        get_random = self.database.get_table_db()
+        all_table_db = []
+        for item in range(len(get_random)):
+            all_table_db.append(get_random[item])
+        self.history_field.append(str(all_table_db))
+
+    def add_words_in_file_js_db(self):
+        """"Добавление в фаил jsan, database, вывод сообщения при бодавления."""
+        russian = self.input_text.toPlainText()
+        english = self.output_text.toPlainText()
+        if russian:
+            self.database.add_word_in_db(russian, english)
+            self.history_field.setText(f"Added to the databasie: {russian, english}")
+        self.database.save_to_json('words_js_file.json')
+
+    def start_session(self):
+        """Старт ссесии. Выбор количества вопросов в рандом"""
+        self.session_words = random.sample(list(self.words.items()), self.session_spin.value())
+        self.next_word()
+
+    def next_word(self):
+        """Следующий вопрос из сесии. Выбор корретного ответа. Удаление из переменой с вопросами для ссеси. Вывод сообщении об окончание ссесии """
+        if not self.session_words:
+            self.history_field.append("End of session. Start a new session.")
+            self.ru_text.clear()
+            return
+        self.current_word = self.session_words.pop()
+        self.ru_text.setText(self.current_word[0])
+
+    def check_answer(self):
+        """Проверка на ответ пользователя запущеной ссесие. Вывод информации вопрос ответ."""
+        user_answer = self.en_text.text().strip()
+        correct_answer = self.current_word[1]
+
+        if user_answer.lower() == correct_answer.lower():
+            self.history_field.append(f"Q: {self.current_word[0]}\nA: {user_answer} - Correct!\n")
+        else:
+            self.history_field.append(
+                f"Q: {self.current_word[0]}\nA: {user_answer} - Incorrect, Correct Answer: {correct_answer}\n")
+
+        self.en_text.clear()
+        self.next_word()
+
+    def translate_text(self):
+        """"Преревод текста"""
+        input_text = self.input_text.toPlainText()
+        if input_text.strip():
+            try:
+# Синхронный вызов для версии 3.1.0a0
+                translated_text = self.translator.translate(input_text, src='ru', dest='en').text
+                self.output_text.setPlainText(translated_text)
+            except Exception as e:
+                self.output_text.setPlainText(f"Translation error: {str(e)}")
+        else:
+            self.output_text.clear()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = TranslatorApp()
+    window.show()
+    sys.exit(app.exec())
